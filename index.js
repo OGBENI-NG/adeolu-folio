1a64097097c045836f7619cf6ccc470f52e145c1
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-03-20 16:12:15 +0100

    update.css

--- index.js
+++ index.js
@@ -1,45 +1,140 @@
-import animation from "./animation.js"
+import animation from "./utility.js"
+import { workData } from "./workData.js"
 const navbarToggle = document.getElementById("navbar-toggle")
 const sidenav = document.getElementById("sidenav")
+const overlay = document.getElementById("overlay")
+const showMoreBtn = document.getElementById("show-more-btn")
+const worksContainer = document.getElementById("works-container") 
+
+let projectsToShow = 4
+const maxProjectsToShow = workData.length
+
 //Navbar 
 navbarToggle.addEventListener("click", () => {
   if(navbarToggle.classList.toggle("active")){
     sidenav.style.width = "60%"
+    sidenav.style.opacity = "1"
+    overlay.style.width = "100%"
   } else {
     sidenav.style.width = "0"
+    overlay.style.width = "0"
+    sidenav.style.opacity = "0"
+
   }
   
 })
-// Name, intro animation
-animation(); // Call the animation function once when the page loads
-setInterval(animation, 2000); // Call the animation function every 2 seconds
-
-
 //overlay close
 window.onclick = (e) => {{
-    if(e.target.id === 'overlay') {
-        featuresOverlay.style.width = "0%"
-    }
+  if(e.target.id === 'overlay') {
+    navbarToggle.classList.toggle("active")
+    overlay.style.width = "0"
+    sidenav.style.width = "0"
+    sidenav.style.opacity = "0"
+  }
 }}
+// Name, intro animation
+animation() // Call the animation function once when the page loads
+setInterval(animation, 2000) // Call the animation function every 2 seconds
 
-let containerEl = document.querySelector(".portfolio-item");
-// 2. Passing the configuration object inline
-//https://www.kunkalabs.com/mixitup/docs/configuration-object/
-let mixer = mixitup(containerEl, {
-  animation: {
-    effects: "fade translateZ(-100px)",
-    effectsIn: "fade translateY(-100%)",
-    easing: "cubic-bezier(0.645, 0.045, 0.355, 1)"
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.like) {
+    handleLike(e.target.dataset.like)
+  } else if  (e.target.dataset.dislike) {
+    dislikeClick(e.target.dataset.dislike)
   }
-});
-// fancybox insilaze & options //
-$("[data-fancybox]").fancybox({
-  loop: true,
-  hash: true,
-  transitionEffect: "slide",
-  /* zoom VS next////////////////////
-  clickContent - i modify the deafult - now when you click on the image you go to the next image - i more like this approach than zoom on desktop (This idea was in the classic/first lightbox) */
-  clickContent: function(current, event) {
-    return current.type === "image" ? "next" : false;
+})
+
+function handleLike(likeId) {
+  const targetLikeObj = workData.find((like) => like.uuid === likeId)
+
+  if (targetLikeObj.isLike) {
+    // Already liked, toggle to unliked
+    targetLikeObj.likes--
+    targetLikeObj.isLike = false
+  } else {
+    // Not liked, toggle to liked and undislike if necessary
+    targetLikeObj.likes++
+    targetLikeObj.isLike = true
+
+    if (targetLikeObj.isDislike) {
+      targetLikeObj.dislike--
+      targetLikeObj.isDislike = false
+    }
   }
-});
+
+  render()
+}
+
+function dislikeClick(dislikeId) {
+  const targetDislikeObj = workData.find((dislike) => dislike.uuid === dislikeId)
+
+  if (targetDislikeObj.isDislike) {
+    // Already disliked, toggle to undisliked
+    targetDislikeObj.dislike--
+    targetDislikeObj.isDislike = false
+  } else {
+    // Not disliked, toggle to disliked and unlike if necessary
+    targetDislikeObj.dislike++
+    targetDislikeObj.isDislike = true
+
+    if (targetDislikeObj.isLike) {
+      targetDislikeObj.likes--
+      targetDislikeObj.isLike = false
+    }
+  }
+
+  render()
+}
+
+function myWorks() {
+  return workData.slice(0, projectsToShow).map(function ({ img, projectName, uuid, description, projectLink, likes, dislike, isLike, isDislike }){
+    const likeColor = isLike ? 'liked' : ''
+    const dislikeColor = isDislike ? 'disliked' : ''
+
+    return `
+      <div class="work--overlay">
+        <div class="work-img--container">
+          <img src="${img}" alt="work--img" class="work-img">
+        </div>
+        <div class="work--details--container">
+          <div class="link--container">
+            <span class="project--name">${projectName}</span>
+            <div class="like--container">
+              <p class="likes" >${likes}
+              <i class="fa-solid fa-heart ${likeColor}" data-like="${uuid}" ></i>
+
+              </p>
+              <p class="likes">${dislike}
+              <i class="fa-solid fa-thumbs-down ${dislikeColor}" data-dislike="${uuid}"></i>
+
+              </p>
+            </div>
+          </div>
+          <p class="project--details">
+            ${description}
+            <a href="#" class="project--link">${projectLink}</a>
+          </p>
+        </div>
+      </div>
+    `
+  }).join('')
+}
+
+//display project container from 
+showMoreBtn.addEventListener('click', () => {
+  if (projectsToShow === maxProjectsToShow) {
+    projectsToShow = 4
+    showMoreBtn.innerText = 'Show more'
+  } else {
+    projectsToShow = maxProjectsToShow
+    showMoreBtn.innerText = 'Show less'
+  }
+  render()
+})
+
+function render() {
+  document.getElementById('works-container').innerHTML = myWorks()
+}
+render()
+
+
---
+++ workData.js
@@ -0,0 +1,102 @@
+import { v4 as uuids4 } from 'https://jspm.dev/uuid'
+const workData = [
+    {
+        img: './worksimg/work.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 26,
+        dislike: 2,
+        isLike: false,
+        isDislike: false
+        
+    },
+    
+    {
+        img: './worksimg/work1.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 56,
+        dislike: 2,
+        isLike: false,
+        isDislike: false
+        
+    },
+    {
+        img: './worksimg/work2.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 68,
+        dislike: 6,
+        isLike: false,
+        isDislike: false
+        
+    },
+    {
+        img: './worksimg/work3.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 72,
+        dislike: 11,
+        isLike: false,
+        isDislike: false
+        
+    },
+    {
+        img: './worksimg/work4.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 11,
+        dislike: 3,
+        isLike: false,
+        isDislike: false
+        
+    },
+    {
+        img: './worksimg/work5.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 67,
+        dislike: 4,
+        isLike: false,
+        isDislike: false
+        
+    },
+    {
+        img: './worksimg/work4.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 11,
+        dislike: 3,
+        isLike: false,
+        isDislike: false
+        
+    },
+    {
+        img: './worksimg/work1.jpg',
+        projectName: 'project one',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ad numquam iusto quia fuga veniam!',
+        projectLink: "project link",
+        uuid: uuids4(),
+        likes: 56,
+        dislike: 2,
+        isLike: false,
+        isDislike: false
+        
+    }
+
+]
+export {workData}
